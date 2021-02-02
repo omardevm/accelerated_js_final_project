45ff15f6648d6253990158272b03f5738044d390
work <omardguez.contacto@gmail.com>
2021-02-02 14:34:46 -0600

    XMLHttpRequest method

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 searchButton.addEventListener("click", () => {
-  searchWeather(searchCity.value);
+  searchWeather(searchInput.value);
 });
 
 function searchWeather(str) {
   if (str.trim().length == 0) {
     return alert("Please enter a city name");
   } else {
-    const url = `${base}/weather?q=${str}`;
+    const url = `${base}/weather?q=${str}&units=metric&appid=${key}`;
     http.open("GET", url);
     http.onreadystatechange = function () {
       if (http.readyState === XMLHttpRequest.DONE && http.status == 200) {
-          var data = JSON.parse(http.responseText);
-          console.log(data);
+        var data = JSON.parse(http.responseText);
+        var weatherData = new Weather(
+          str,
+          data.weather[0].description.toUpperCase()
+        );
+        weatherData.temperature = data.main.temp;
+        updateWeather(weatherData);
       } else if (http.readyState === XMLHttpRequest.DONE) {
         return alert("Error on comunication");
       }
     };
+    http.send();
   }
+}
+
+function updateWeather(weatherData) {
+  weatherCity.textContent = weatherData.cityName;
+  weatherDescription.textContent = weatherData.description;
+  weatherTemperature.textContent = weatherData.temperature;
 }
